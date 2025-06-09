bd6460039a1d58026a468a6ff65ba59573a98bd3
chadgamer <papamclester@gmail.com>
2025-06-09 16:40:25 +0800

    .

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -46,8 +46,8 @@ export default function Aboutme () {
                                 <svg class="w-9 sm:w-14 h-9 sm:h-14" xmlns="http://www.w3.org/2000/svg" fill="#FFF" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                                 </svg>
-                                <span class="block mt-3 text-xl font-bold text-black sm:mt-6 sm:text-4xl lg:text-5xl"> 2,984 </span>
-                                <span class="block mt-2 text-sm font-medium leading-snug text-amber-900 sm:text-base"> Customer chat<br />served on July </span>
+                                <span class="block mt-3 text-xl font-bold text-black sm:mt-6 sm:text-4xl lg:text-5xl"> 2 </span>
+                                <span class="block mt-2 text-sm font-medium leading-snug text-amber-900 sm:text-base"> Years of<br />experience </span>
                             </div>
                         </div>
                     </div>
